da898508c7ba3fbc3d110fb80a18192acb0cb72b
bvvivek6 <vivekbv242813@gmail.com>
2025-07-01 21:38:04 +0530

    Minor fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
 
   return (
     <>
-      <div className="relative h-[100vh] bg-[#0E0E0E] flex flex-col tracking-tight items-center justify-center text-center px-4 overflow-hidden">
+      <div className="relative h-[90vh] bg-[#0E0E0E] flex flex-col tracking-tight items-center justify-center text-center px-4 overflow-hidden">
         {showCurtain && (
           <>
             <motion.div
